e92c8c0e302cae0f67a18c2ea9ac97bfea7972b6
Luka Peschke <mail@lukapeschke.com>
2020-10-24 15:03:03 +0200

    feat(repl): Basic raw mode + support for clear
    Signed-off-by: Luka Peschke <mail@lukapeschke.com>

--- src/repl.rs
+++ src/repl.rs
@@ -2,13 +2,33 @@ use crate::evaluator;
 use crate::lexer;
 use crate::parser;
 
-use std::io::Write;
+use std::io::{stdin, stdout, Stdout, Write};
+use termion::cursor;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
 
-static PROMPT: &str = "~> ";
+static PROMPT: &str = "\r~> ";
 
 struct Repl {
     ev: evaluator::Evaluator,
     debug: bool,
+    stdout: RawTerminal<Stdout>,
+}
+
+#[derive(Debug)]
+enum NextLineStatus {
+    Continue,
+    Stop,
+}
+
+impl NextLineStatus {
+    pub fn should_stop(&self) -> bool {
+        match self {
+            NextLineStatus::Continue => false,
+            NextLineStatus::Stop => true,
+        }
+    }
 }
 
 impl Repl {
@@ -16,12 +36,28 @@ impl Repl {
         Repl {
             ev: evaluator::Evaluator::new(),
             debug,
+            stdout: stdout().into_raw_mode().unwrap(),
         }
     }
+
+    fn print(&mut self, s: &str) -> Result<(), String> {
+        if let Err(e) = self.stdout.write(s.as_bytes()) {
+            return Err(format!("error while writing to stdout: {}", e));
+        }
+        if let Err(e) = self.stdout.flush() {
+            return Err(format!("error while flushing stdout: {}", e));
+        }
+        Ok(())
+    }
+
+    fn println(&mut self, s: &str) -> Result<(), String> {
+        self.print(&format!("{}\r\n", s))
+    }
+
     fn handle_line(&mut self, line: &str) -> Result<(), String> {
         if let Some(mut lex) = lexer::Lexer::new(line) {
             if self.debug {
-                println!("Tokens are {:#?}", lex.get_all_tokens())
+                self.println(&format!("Tokens are {:#?}", lex.get_all_tokens()))?;
             }
             let mut parser;
             if let Some(p) = parser::Parser::new(&mut lex) {
@@ -32,7 +68,7 @@ impl Repl {
 
             let prog = parser.parse_program()?;
             if self.debug {
-                println!("Program parses to: {:?}", prog);
+                self.println(&format!("Program parses to: {:?}", prog))?;
             }
             let result = self.ev.eval(prog.as_node(), &None)?.repr();
             println!("Program evaluates to: {:?}", result);
@@ -42,30 +78,66 @@ impl Repl {
         }
     }
 
-    pub fn run(&mut self) -> Result<(), String> {
-        let mut buf = String::new();
-        loop {
-            print!("{}", PROMPT);
-            std::io::stdout().flush().unwrap();
-            buf.clear();
-            match std::io::stdin().read_line(&mut buf) {
-                // EOF
-                Ok(0) => {
-                    println!("bye!");
-                    return Ok(());
-                }
-                // Normal case
-                Ok(_) => {
-                    if let Err(s) = self.handle_line(&buf) {
-                        eprintln!("Error: {}", s)
+    fn clear(&mut self) -> Result<(), String> {
+        self.print(&format!("{}{}", termion::clear::All, cursor::Goto(1, 1)))
+    }
+
+    fn write_prompt(&mut self) -> Result<(), String> {
+        self.print(PROMPT)
+    }
+
+    fn display_line(&mut self, line: &str, newline: bool) -> Result<(), String> {
+        let formatted_line = format!("{}{}", PROMPT, line);
+        match newline {
+            true => self.println(&formatted_line),
+            false => self.print(&formatted_line),
+        }
+    }
+
+    fn next_line(&mut self, buf: &mut String) -> Result<NextLineStatus, String> {
+        let stdin = stdin();
+        for key_result in stdin.keys() {
+            match key_result {
+                Ok(k) => match k {
+                    Key::Char(c) => match c {
+                        '\n' => {
+                            self.display_line(&buf, true)?;
+                            return Ok(NextLineStatus::Continue);
+                        }
+                        _ => buf.push(c),
+                    },
+                    Key::Ctrl('d') | Key::Ctrl('c') | Key::Ctrl('q') => {
+                        return Ok(NextLineStatus::Stop)
                     }
-                }
-                // Oops
-                Err(e) => {
-                    return Err(e.to_string());
+                    Key::Ctrl('l') => self.clear()?,
+                    _ => (),
+                },
+                Err(e) => return Err(format!("error while reading stdin: {}", e)),
+            }
+            self.display_line(&buf, false)?;
+        }
+        Ok(NextLineStatus::Stop)
+    }
+
+    fn loop_(&mut self) -> Result<(), String> {
+        let mut buf = String::new();
+        let mut stop = NextLineStatus::Continue;
+        while !stop.should_stop() {
+            if !buf.is_empty() {
+                if let Err(e) = self.handle_line(&buf) {
+                    self.println(&format!("Error: {}", e))?;
                 }
             }
+            buf.clear();
+            self.write_prompt()?;
+            stop = self.next_line(&mut buf)?;
         }
+        self.println("bye!")?;
+        Ok(())
+    }
+
+    pub fn run(&mut self) -> Result<(), String> {
+        self.loop_()
     }
 }
 
